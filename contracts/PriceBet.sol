3326b2eeb029fa9ec38caefc5542639c45d01714
Orthae <orthaefluorite@gmail.com>
2024-09-07 01:17:55 +0200

    use pull redstone

--- contracts/PriceBet.sol
+++ contracts/PriceBet.sol
@@ -1,8 +1,8 @@
-import "./AggregatorV3Interface.sol";
+import "@redstone-finance/evm-connector/contracts/data-services/MainDemoConsumerBase.sol";
 
-contract PriceBet {
-    AggregatorV3Interface public redstoneOracle;
-    int256 public expected;
+contract PriceBet is MainDemoConsumerBase{
+    bytes32 symbol;
+    uint256 public expected;
     int256 public outcome = 0;
     uint256 public end;
     uint256 public endLimit;
@@ -13,21 +13,23 @@ contract PriceBet {
     uint256 public noPool;
 
     constructor(
-        address _redstoneOracle, 
-        int256 _expected,
+        bytes32 _symbol,
+        uint256 _expected,
         uint256 _end
     ) {
-        redstoneOracle = AggregatorV3Interface(_redstoneOracle);
+        symbol = _symbol;
         expected = _expected;
         end = _end;
     }
     
+    function validateTimestamp(uint256 receivedTimestampMilliseconds) public view virtual override { }
+
     function finalize() external {
         // require(block.timestamp > end, "In progress");
         // require(block.timestamp < (end + endLimit), "Passed the limit");
         require(outcome == 0, "Already finalized");
 
-        (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound) = redstoneOracle.latestRoundData();
+        uint256 answer = getOracleNumericValueFromTxMsg(symbol);
         if(answer >= expected) {
             outcome = 1;
         } else {
